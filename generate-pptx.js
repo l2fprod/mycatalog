f24daac8a8b4235ea4fbf9381db072b206e6bd58
Lionel Mace <lionel.mace@fr.ibm.com>
2016-02-25 17:59:31 +0100

    Generate ppt.

---
+++ generate-pptx.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+var path = require('path');
+
+var services = JSON.parse(fs.readFileSync('public/data/services.json', 'utf8'));
+var officegen = require('officegen');
+var pptx = officegen('pptx');
+
+pptx.setWidescreen(true);
+
+services.forEach(function (service) {
+    if (!service.entity.active) {
+        return;
+    }
+
+    // New slide
+    slide = pptx.makeNewSlide();
+
+    try {
+        slide.addImage(path.resolve(__dirname, 'public/data/icons/' + service.metadata.guid + '.png'),
+                       {x: 50, y: 30});
+    } catch (err) {}
+
+    var extra = service.entity.extra;
+    if (extra && extra.displayName) {
+        slide.addText(extra.displayName,         {x: 150, y: 30,  cx: '100%', font_size: 30, bold: true });
+        
+        slide.addText("Short Description:",      {x: 100, y: 150, cx: '600', font_size: 20, bold: true });
+        slide.addText(service.entity.description,{x: 100, y: 180, cx: '1100', font_size: 20});
+        
+        slide.addText("Long Description:",       {x: 100, y: 300, cx: '100%', font_size: 20, bold: true });
+        slide.addText(extra.longDescription,     {x: 100, y: 330, cx: '1100', font_size: 20});
+        
+        slide.addText("Author: ",                {x: 100, y: 530, cx: '100%', font_size: 20, bold: true });
+        slide.addText(extra.providerDisplayName, {x: 100, y: 560, cx: '100%'});
+        
+        slide.addText(extra.documentationUrl,    {x: 100, y: 600, cx: '100%', cy: 20, color: '0000ff'});
+    } else {
+        slide.addText(service.entity.label,      {x: 150, y: 30,  cx: '100%', font_size: 30, bold: true});
+        
+        slide.addText("Short Description:",      {x: 100, y: 150, cx: '600', font_size: 20, bold: true });
+        slide.addText(service.entity.description,{x: 100, y: 180, cx: '100%', font_size: 20});
+        
+        //bug slide.addText(service.entity.provider,   {x: 100, y: 300, cx: '100%'});
+    }
+
+});
+
+var out = fs.createWriteStream('public/data/catalog.pptx');
+pptx.generate(out);
