75e2f315984741f64f966a6da7dd4242069cb82b
Lionel Mace <lionel.mace@fr.ibm.com>
2016-03-14 18:26:13 +0100

    New intro slide and metadata panel for each service. Added region, status, free plan in Office docs

--- export2office.js
+++ export2office.js
@@ -3,6 +3,7 @@ var path = require('path');
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
+var vm = require('vm');
 
 var services = JSON.parse(fs.readFileSync('public/generated/services.json', 'utf8'));
 var officegen = require('officegen');
@@ -11,6 +12,13 @@ var officegen = require('officegen');
 var date = new Date();
 var dateYMD = moment(date).format('YYYY-MM-DD');
 var dateMDY = moment(date).format('MMMM DD, YYYY');
+
+// Added to loop through the region
+var script = vm.createScript(fs.readFileSync('./public/js/bluemix-configuration.js'));
+var sandbox = {};
+script.runInNewContext(sandbox);
+var regions = sandbox.regions;
+//
 
 router.post('/:format', function (req, res) {
     var servicesToExport;
@@ -68,12 +76,11 @@ function exportToExcel(services, res) {
         sheet.data[0][3] = "Author";
         sheet.data[0][4] = "Status";
         sheet.data[0][5] = "Free Plan";
-        //sheet.data[0][5] = "Regions";
-        
-        sheet.data[0][6] = "Creation Date";
-        sheet.data[0][7] = "Last Modification";
-        sheet.data[0][8] = "Long Description";
-        sheet.data[0][9] = "URL";
+        sheet.data[0][6] = "Regions";
+        sheet.data[0][7] = "Creation Date";
+        sheet.data[0][8] = "Last Modification";
+        sheet.data[0][9] = "Long Description";
+        sheet.data[0][10] = "URL";
 
         // Cell Content
         sheet.data[row] = [];
@@ -84,19 +91,35 @@ function exportToExcel(services, res) {
             sheet.data[row][1] = service.entity.tags[0];
             sheet.data[row][2] = service.entity.description;
             sheet.data[row][3] = extra.providerDisplayName;
-            sheet.data[row][8] = extra.longDescription;
-            sheet.data[row][9] = extra.documentationUrl;
+            sheet.data[row][9] = extra.longDescription;
+            sheet.data[row][10] = extra.documentationUrl;
         } else {
             sheet.data[row][0] = service.entity.label;
             sheet.data[row][1] = service.entity.tags[0];
             sheet.data[row][2] = service.entity.description;
             sheet.data[row][3] = service.entity.provider;
         }
-        sheet.data[row][4] = ""; //TODO
-        sheet.data[row][5] = ""; //TODO
-        
-        sheet.data[row][6] = service.metadata.created_at;
-        sheet.data[row][7] = service.metadata.updated_at;
+
+        var status = "";
+        if (service.entity.tags.indexOf('ibm_beta') >= 0)
+            status = "Beta";
+        else if (service.entity.tags.indexOf('ibm_experimental') >= 0)
+            status = "Experimental";
+        else
+            status = "Production Ready";
+        sheet.data[row][4] = status;
+        sheet.data[row][5] = (service.entity.tags.indexOf("custom_has_free_plan") >= 0) ? "Yes" : "No";
+
+        var datacenter = "";
+        for (var region in regions) {
+            if (service.entity.tags.indexOf(regions[region].tag) >= 0) {
+                datacenter += regions[region].label + " ";
+            }
+        }
+        sheet.data[row][6] = datacenter;
+
+        sheet.data[row][7] = moment(service.metadata.created_at).format('YYYY-MM-DD');
+        sheet.data[row][8] = moment(service.metadata.updated_at).format('YYYY-MM-DD');
 
         row++;
     });
@@ -114,7 +137,7 @@ function exportToPowerpoint(services, res) {
 
     pptx.setWidescreen(true);
 
-    // Intro slide
+    // Intro slide --------------------
     slide = pptx.makeNewSlide();
     slide.back = '1E3648';
     slide.addText("My Bluemix Catalog", {
@@ -133,19 +156,11 @@ function exportToPowerpoint(services, res) {
         bold: true,
         color: 'ffffff'
     });
-    slide.addText("This catalog uses the Cloudfoundry API to retrieve data from the Bluemix US catalog.", {
-        x: 150,
-        y: 400,
-        cx: '800',
-        font_size: 30,
-        bold: false,
-        color: '808080'
-    });
-    slide.addText("It tries to be as accurate as possible. Use with care.", {
+    slide.addText("The list of services in this document was extracted the Bluemix catalog using the public Cloud Foundry API. This content is accurate as possible. Yet, use with care and refer to the official Bluemix catalog www.bluemix.net/catalog.", {
         x: 150,
         y: 500,
         cx: '900',
-        font_size: 30,
+        font_size: 20,
         bold: false,
         color: '808080'
     });
@@ -155,8 +170,9 @@ function exportToPowerpoint(services, res) {
         cx: 90,
         cy: 100
     });
+    // Intro slide END
 
-
+    // One slide per service
     services.forEach(function (service) {
         if (!service.entity.active) {
             return;
@@ -205,11 +221,18 @@ function exportToPowerpoint(services, res) {
                 font_size: 18
             });
 
-            slide.addShape("rect", {x: 830, y: 230, cx: 350 , cy: 300, fill: '47A9C0'});
+            // Metada rectangle panel on the right hand side
+            slide.addShape("rect", {
+                x: 830,
+                y: 230,
+                cx: 390,
+                cy: 300,
+                fill: '47A9C0'
+            });
             slide.addText("Author: ", {
                 x: 850,
                 y: 250,
-                cx: '100%',
+                cx: '150',
                 font_size: 18,
                 bold: false,
                 color: 'ffffff'
@@ -217,14 +240,15 @@ function exportToPowerpoint(services, res) {
             slide.addText(extra.providerDisplayName, {
                 x: 1000,
                 y: 250,
-                cx: '200',
+                cx: '220',
                 font_size: 18,
-                color: 'ffffff'
+                color: 'ffffff',
+                bold: false
             });
             slide.addText("Category: ", {
                 x: 850,
                 y: 300,
-                cx: '100%',
+                cx: '150',
                 font_size: 18,
                 bold: false,
                 color: 'ffffff'
@@ -234,60 +258,81 @@ function exportToPowerpoint(services, res) {
                 y: 300,
                 cx: '200',
                 font_size: 18,
-                color: 'ffffff'
+                color: 'ffffff',
+                bold: false
             });
-            slide.addText("State: ", {
+            slide.addText("Status: ", {
                 x: 850,
                 y: 350,
-                cx: '100%',
+                cx: '150',
                 font_size: 18,
                 color: 'ffffff'
             });
-            slide.addText("TODO", {
+
+            var status = "";
+            if (service.entity.tags.indexOf('ibm_beta') >= 0)
+                status = "Beta";
+            else if (service.entity.tags.indexOf('ibm_experimental') >= 0)
+                status = "Experimental";
+            else
+                status = "Production Ready";
+            slide.addText(status, {
                 x: 1000,
                 y: 350,
                 cx: '200',
                 font_size: 18,
-                color: 'ffffff'
+                color: 'ffffff',
+                bold: false
             });
             slide.addText("Free Plan: ", {
                 x: 850,
                 y: 400,
-                cx: '100%',
+                cx: '150',
                 font_size: 18,
                 color: 'ffffff'
             });
-            slide.addText("Yes/No", {
+            var isFreePlan = (service.entity.tags.indexOf("custom_has_free_plan") >= 0) ? "Yes" : "No";
+            slide.addText(isFreePlan, {
                 x: 1000,
                 y: 400,
                 cx: '200',
                 font_size: 18,
-                color: 'ffffff'
+                color: 'ffffff',
+                bold: false
             });
             slide.addText("Regions: ", {
                 x: 850,
                 y: 450,
-                cx: '100%',
+                cx: '150',
                 font_size: 18,
                 color: 'ffffff'
             });
-            slide.addText("Yes/No", {
+            var datacenter = "";
+            for (var region in regions) {
+                if (service.entity.tags.indexOf(regions[region].tag) >= 0) {
+                    datacenter += regions[region].label + " ";
+                }
+            }
+            slide.addText(datacenter, {
                 x: 1000,
                 y: 450,
                 cx: '200',
                 font_size: 18,
-                color: 'ffffff'
+                color: 'ffffff',
+                bold: false
             });
+            // Metada rectangle panel on the right hand side END
 
+            // Footer -------------------------------
             slide.addText(extra.documentationUrl, {
                 x: 100,
                 y: 600,
                 cx: '100%',
-                cy: 20,
+                cy: 40,
                 color: '0000ff'
             });
 
-            slide.addText(slide.getPageNumber()+1, {
+            slide.addText(slide.getPageNumber() + 1, {
                 x: 1150,
                 y: 630,
                 cx: '100',
@@ -309,10 +354,7 @@ function exportToPowerpoint(services, res) {
                 cx: '100%',
                 font_size: 20
             });
-
-            //bug slide.addText(service.entity.provider,   {x: 100, y: 300, cx: '100%'});
         }
-
     });
 
     res.setHeader("Content-Type", "application/octet-stream");
