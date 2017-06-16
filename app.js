bbe721406afc7821b65eab808b0a165d00010be4
leonflopez <leonfell@ie.ibm.com>
2017-06-16 12:30:27 +0100

    modified cronTime in app.js to run the task every minute

--- app.js
+++ app.js
@@ -65,7 +65,8 @@ function scheduleUpdater() {
   var CronJob = require('cron').CronJob;
   new CronJob({
     // run twice, once at 8 in the US but also at 8 in Europe
-    cronTime: '0 0 8,23 * * *',
+    // cronTime: '0 0 8,23 * * *',
+	cronTime: '* * * * *',
     onTick: function () {
       console.log(new Date(), "Updating services...");
       serviceUpdater.run(saveSnapshotCallback);
