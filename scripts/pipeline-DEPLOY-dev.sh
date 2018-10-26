62f4b6e8cf1cf1af3ec3e83a0ac93e21f76bac06
Frederic Lavigne <fred@L2FProd.com>
2018-10-26 11:18:49 +0200

    script for pipeline

---
+++ scripts/pipeline-DEPLOY-dev.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Prepare env
+npm config delete prefix
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 6.9.1
+
+# Retrieve resources
+npm install
+./retrieve.sh
+rm -rf node_modules public/vendor
+
+# Push the app
+if ! cf app $CF_APP; then
+  cf push $CF_APP -n $CF_APP -f dev-manifest.yml
+else
+  OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
+  rollback() {
+    set +e
+    if cf app $OLD_CF_APP; then
+      cf logs $CF_APP --recent
+      cf delete $CF_APP -f
+      cf rename $OLD_CF_APP $CF_APP
+    fi
+    exit 1
+  }
+  set -e
+  trap rollback ERR
+  cf rename $CF_APP $OLD_CF_APP
+  cf push $CF_APP -n $CF_APP -f dev-manifest.yml
+  cf delete $OLD_CF_APP -f
+fi
