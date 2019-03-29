47356a2947163705bee3a4e9b00fd9cd087d341a
Frederic Lavigne <fred@L2FProd.com>
2019-03-29 17:52:49 +0100

    bump node

--- scripts/pipeline-DEPLOY-dev.sh
+++ scripts/pipeline-DEPLOY-dev.sh
@@ -3,7 +3,7 @@
 npm config delete prefix
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install 6.9.1
+nvm install 8.9.4
 
 # Retrieve resources
 npm install
