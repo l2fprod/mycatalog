2b827c1bc96281b8494fed146f5ae549cd171b61
Frederic Lavigne <frederic.lavigne@ibm.com>
2021-05-27 08:19:35 -0500

    base

---
+++ ui/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>My Catalog</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid style="height: calc(100vh - 56px - 20px);">
+        <v-layout fill-height>
+          <ProductTable/>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ProductTable from './components/ProductTable';
+
+export default {
+  name: 'App',
+
+  components: {
+    ProductTable,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
